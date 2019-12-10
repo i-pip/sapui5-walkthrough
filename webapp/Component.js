33e775a809e16070d1c00511d0f18e43739c89d7
i-pip <jrphiri@gmail.com>
2019-12-10 10:30:47 +0000

    Made HelloDialog resusable

--- webapp/Component.js
+++ webapp/Component.js
@@ -1,8 +1,9 @@
 sap.ui.define([
 	"sap/ui/core/UIComponent",
 	"sap/ui/model/json/JSONModel",
-	"sap/ui/model/resource/ResourceModel"
-], function(UIComponent, JSONModel, ResourceModel){
+	"sap/ui/model/resource/ResourceModel",
+	"./controller/HelloDialog"
+], function(UIComponent, JSONModel, ResourceModel, HelloDialog){
 	"use strict";
 	
 	return UIComponent.extend("sap.ui.demo.walkthrough.Component", {
@@ -28,11 +29,23 @@ sap.ui.define([
 			var oModel = new JSONModel(oData);
 			this.setModel(oModel);
 			
+			// set dialog 
+			this._helloDialog = new HelloDialog(this.getRootControl());
+			
 			// set i18n model
-			var i18nModel = new ResourceModel({
-				bundleName: "sap.ui.demo.walkthrough.i18n.i18n"
-			});
-			this.setModel(i18nModel, "i18n");
+			// var i18nModel = new ResourceModel({
+			// 	bundleName: "sap.ui.demo.walkthrough.i18n.i18n"
+			// });
+			// this.setModel(i18nModel, "i18n");
+		},
+		
+		exit: function() {
+			this._helloDialog.destroy();
+			delete this._helloDialog;
+		},
+		
+		openHelloDialog : function() {
+			this._helloDialog.open();
 		}
 	});
 });
